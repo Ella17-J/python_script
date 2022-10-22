fc2e40f893f0ae7fd75e9c065e47c2fc964c6a9f
Ella17-J <85752048+Ella17-J@users.noreply.github.com>
2022-10-22 09:38:39 -0700

    Add new pizza file

---
+++ pizza.py
@@ -0,0 +1,11 @@
+fav_pizza = ['Meat pizza', 'Pepperoni pizza', 'Veggie pizza ']
+for fav_pizza in fav_pizza:
+    print(fav_pizza)
+    print("I like " + fav_pizza + "\n")
+print("I really love pizza!" + "\n")
+
+pets = ["cats", "dogs", "parrots"]
+for pets in pets:
+    print(pets)
+    print(pets.title() + " make great pets." + "\n")
+print("Any of these animals would make a great pet!")
